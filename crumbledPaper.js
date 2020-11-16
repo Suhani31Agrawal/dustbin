cd564873d00a0d1e291c8af2c515f5097c2bbe36
Suhani31Agrawal <73304019+Suhani31Agrawal@users.noreply.github.com>
2020-11-16 22:49:46 +0530

    Add files via upload

---
+++ crumbledPaper.js
@@ -0,0 +1,23 @@
+class CrumbledPaper {
+    constructor(x,y,r) {
+      var options = {
+          isStatic: false,
+          restitution:0.1,
+          friction:0.8,
+          density:1.2
+      }
+      this.image=loadImage("paper.png");
+      this.body = Bodies.circle(x,y,r,options);
+      this.r = r;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y)
+      imageMode(CENTER);
+      fill("yellow");
+      image(this.image,0, 0, 40, 40);
+      pop();
+    }
+  };
